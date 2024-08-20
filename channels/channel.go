96b0500c0e1309c6bcc88f7615413e9d055ba69e
Rohit singh <raj0eshsing0909@gmail.com>
2024-08-20 21:23:59 +0530

    channels

--- channels/channel.go
+++ channels/channel.go
@@ -5,16 +5,44 @@ import (
 	"time"
 )
 
-func ProcessNum(result chan int) {
-	fmt.Println("We are Processing the number to result => ", <-result)
+// func ProcessNum(result chan int) {
+// 	fmt.Println("We are Processing the number to result => ", <-result)
+// }
+
+func worker(work chan bool) {
+	fmt.Println("working .....")
+	time.Sleep(time.Second * 3)
+	fmt.Println("done !")
+	work <- true
 }
 
 func main() {
-	fmt.Println("channels in go")
-	result := make(chan int)
-	go ProcessNum(result)
-	result <- 44
-	time.Sleep(time.Second * 2)
+	// fmt.Println("channels in go")
+	// result := make(chan int)
+	// go ProcessNum(result)
+	// result <- 44
+	// time.Sleep(time.Second * 2)
+
+
+
+	// channel buffering
+
+	// message := make(chan string , 2)
+
+	// message <- "Ready for buffer"
+	// message <- "Done !"
+
+	// fmt.Println(<-message)
+	// fmt.Println(<-message)
+
+	// channel synchronization
+
+	work := make(chan bool, 1)
+	go worker(work)
+	<-work	
+
+
+
 
 	
 }
